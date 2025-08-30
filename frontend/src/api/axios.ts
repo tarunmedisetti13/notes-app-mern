c1ecbb0032aad45e3cbbcd1a4c85fb4a7e420282
tarun <tarunmedisetti13@gmail.com>
2025-08-30 17:43:28 +0530

    Setup env varibles

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api", // backend URL
+    baseURL: import.meta.env.VITE_BACKEND_URI, // backend URL
 });
 
 // Attach token automatically if available
